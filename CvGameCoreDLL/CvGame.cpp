08623317d33d0ded781f2d56baea522d570b9533
DarkLunaPhantom <darklunaphantom@runbox.com>
2017-04-20 02:22:48 -0500

    Fixed minor error.

--- CvGameCoreDLL/CvGame.cpp
+++ CvGameCoreDLL/CvGame.cpp
@@ -2222,7 +2222,7 @@ again:
 	// DarkLunaPhantom begin - Fixed bug in K-Mod Extended C2C minimize AI turn time option, multiplayer
 	// needs to be checked first because later checks crash pitboss as it is not a valid active player.
 	// K-Mod Extended
-	if (!isNetworkMultiPlayer() && (!GET_PLAYER(getActivePlayer().isTurnActive() || GET_PLAYER(getActivePlayer().isAutoMoves()) && !GET_PLAYER(getActivePlayer().hasBusyUnit() && getBugOptionBOOL("MainInterface__MinimizeAITurnSlices", false))
+	if (!isNetworkMultiPlayer() && (!GET_PLAYER(getActivePlayer()).isTurnActive() || GET_PLAYER(getActivePlayer()).isAutoMoves()) && !GET_PLAYER(getActivePlayer()).hasBusyUnit() && getBugOptionBOOL("MainInterface__MinimizeAITurnSlices", false))
 		{
 			updateTimers();
 			goto again;
