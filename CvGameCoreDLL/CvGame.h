aa41d9a6a2ee2d1028b04a3424fd04c43764fd96
DarkLunaPhantom <darklunaphantom@runbox.com>
2023-01-06 03:40:20 +0100

    PitBoss slowdown is delayed by 5 minutes after login attempt.

--- CvGameCoreDLL/CvGame.h
+++ CvGameCoreDLL/CvGame.h
@@ -587,6 +587,9 @@ public:
 	// PB Mod END
 
 	std::set<int> m_ActivePlayerCycledGroups; // K-Mod. This is used to track which groups have been cycled through in the current turn. Note: it does not need to be kept in sync for multiplayer games.
+    
+    void setSleepDelayMillisecs(int iSleepDelayMillisecs);
+    int getSleepDelayMillisecs() const;
 
 protected:
 	int m_iElapsedGameTurns;
@@ -691,6 +694,8 @@ protected:
 
 	int		m_iNumCultureVictoryCities;
 	int		m_eCultureVictoryCultureLevel;
+    
+    int m_iSleepDelayMillisecs;
 
 	void doTurn();
 	void doDeals();
